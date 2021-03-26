599a4e2867d4cee902d67f68090a67aed3fd289b
Limon96 <nicker08@inbox.ru>
2021-03-26 22:15:45 +0300

    Натяжка верстки

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy("icon");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("image_web");
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("popups_vimeo");
+    eleventyConfig.addPassthroughCopy("sass");
 };
